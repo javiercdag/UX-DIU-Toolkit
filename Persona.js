98f1f945aa4e99926cf9b26b4cdd78ada0ace826
Mumo <56559705+Miguemumo@users.noreply.github.com>
2020-03-06 16:48:47 +0100

    Actualizacion de la bio de Evaristo

--- Persona.js
+++ Persona.js
@@ -68,24 +68,24 @@ angular.module("angular", [])
                 /*************************************/
                 
                 
-				Id: 1,
-				Name: "Evaristo García",
+				Id: 0,
+				Name: "Evaristo",
 				Photo: "evaristo.jpg",
 				Quote: "En los 80 se vivía mejor",
 				Age: 52,
 				Occupation: "Albañil",
 				Family: "Casado durante 20 años y actualmente divorciado",
 				Location: "Granada (Armilla)",
-				Character: "Le gusta pasear con su mujer",
+				Character: "Le gusta pasar el rato en el bar y ver partidos de fútbol",
 				PersonalityTraits: [
 					{ Name: "Introvertido/reservado Vs  Extrov/activo ", Value: 1 },
 					{ Name: "Realista/práctico  Vs    Intuición/imaginativo", Value: 1 },
 					{ Name: "Racional/analitico  Vs   Emocional/impulsivo", Value: 2 },
 					{ Name: "Flemático/apático  Vs   Colérico/visceral", Value: 4 }
 				],
-				Goals: ["Poder evadirse más del trabajo", "Cambiar a un trabajo más remunerado", "Tener compañía"],
+				Goals: ["Poder evadirse más del trabajo", "Cambiar a un trabajo más remunerado", "Compartir sus experiencias vividas con otros"],
 				Frustrations: ["No le gusta la tecnología, suele necesitar ayuda", "No se le da bien conocer gente"],
-				Bio: "Es de Armilla y vive en Granada desde siempre, pero nunca ha tenido grandes oportunidades de trabajo. LLeva 10 años contratado en la empresa actual. Aqui ha hecho buenos amigo en el trabajo y a veces se reúnen en bares",
+				Bio: "Nació en Armilla, se crió en una familia modesta y fue a la escuela hasta 8º de EGB. Tras eso inició la Formación Profesional, pero la dejó para empezar a trabajar como peón en una obra. Nunca ha tenido grandes oportunidades de trabajo, ya que no continuó en su día con los estudios. Estuvo casado durante 20 años, pero por problemas de comunicación se divorció. Como albañil ha hecho algunos amigos y, a veces, se reúnen en bares para pasar el rato",
 				Tech: [
 					{ Name: "TIC/Internet", Value: 1 },
 					{ Name: "Movil", Value: 3 },
@@ -93,15 +93,15 @@ angular.module("angular", [])
 					{ Name: "Software", Value: 1 }
 
 				],
-                Contextos: "Quiere desconectar del trabajo y poder compartir experiencias con otras personas de su edad",
+                Contextos: "Quiere desconectar del trabajo, olvidarse de su relación pasada y poder compartir experiencias con otras personas de su edad",
 				PreferredChannels: [
 					{ Name: "Publicidad Tradicional", Value: 5 },
 					{ Name: "Online & Social Media", Value: 1 },
 					{ Name: "Recomendaciones & sugerencias", Value: 3 },
 					{ Name: "Persona confianza (amigos, boca a boca)", Value: 4 }
 				]
-			}
+			},
+
 		];
 		$scope.model = $scope.Personas[0];
-
 	}])
