349f1a394dd365573fa987d2698e27b063efb35b
javiercdag <58362922+javiercdag@users.noreply.github.com>
2020-03-06 16:13:21 +0100

    Update Journey.js

--- Journey.js
+++ Journey.js
@@ -88,55 +88,55 @@ angular.module("angular", [])
                 /*************************************/
                 
 				Id: 1,
-				Name: "Monica Suarez",
-                Photo: "woman.png",
-                
-				 /*** PASO #1: INSPIRACION ***/ 
-                goal1: "Quiere preparar un viaje con su familia para Verano, tiene sólo 15 dias libres",
-                touch1: "agenda",
-                feel1: "5",
-                con1: "Quiere ir a un pais exotico pero que tenga atracciones para niños pequeños",
-                ima1: "cartoon-going.png",
-                
+				Name: "Evaristo",
+                Photo: "evaristo.jpg",
+    
+                /*** PASO #1: INSPIRACION ***/ 
+                goal1: "quiere compartir piso con alguien para no sentirse solo",
+                touch1: "ordenador",
+                feel1: "3",
+                con1: "es difícil buscar gente con su edad que quiera compartir piso",
+                ima1: "cartoon-planning.png",
+				
                 /*** PASO #2: DECICION ***/ 
-                goal2: "Ir a una agencia de viajes, y decirle sus preferencias y planes",
-                touch2: "Servicio (agencia)",
-                feel2: "4",
-                con2: "Tiene que desplazarse a agencia, explica su intenciones, le llamaran porque no hay nada interesante",
-                ima2: "cartoon-teamthinking.png",
+                goal2: "Busca en internet ofertas para compartir piso",
+                touch2: "ordenador",
+                feel2: "2",
+                con2: "hay muchas páginas para compartir piso, y no sabe cual será buena",
+                ima2: "cartoon-looking.png",
                 
                 /*** PASO #3: ACTUA ***/ 
                 
-                goal3: "Le llaman a los pocos días con un viaje que no le convence",
-                touch3: "Móvil (llamada)",
-                feel3: "2",
-                con3: "Piensa que ha perdido el tiempo",
-                ima3: "cartoon-phoningangry.png",
+                goal3: "Decide entrar en Badi ya que le sale como uno de los primeros resultados",
+                touch3: "ordenador",
+                feel3: "4",
+                con3: "Tiene que registrarse en la página, no se le permite hacerlo sin ese paso",
+                ima3: "cartoon-PCtyping.png",
                 
                 /*** PASO #4: OBSERVA ***/ 
                 
-                goal4: "Busca una oferta en hoteles cerca de playa y con parque atracciones",
-                touch4: "Móvil (webapp)",
+                goal4: "Quiere buscar un piso en el centro de Granada para compartir",
+                touch4: "ordenador",
                 feel4: "2",
-                con4: "No hay mucha información del alojamiento ni de lo que hay alrededor, aunque el precio está bien, va por la calle por lo que está incómoda",
-                ima4: "cartoon-phone-street.png",
+                con4: "No hay ningún tipo de tutorial y no sabe cómo buscar los mejores pisos",
+                ima4: "cartoon-PChard.png",
                 
                  /*** PASO #5: ANALIZA ***/ 
                 
-                goal5: "Reserva a traves de la aplicación ",
-                touch5: "Móvil (webapp)",
-                feel5: "3",
-                con5: "Le pide muchos datos y le resulta incómodo completar formulario",
-                ima5: "cartoon-phone-sitting.png",
-
+                goal5: "Encuentra 2 pisos a compartir que le interesan",
+                touch5: "ordenador",
+                feel5: "2",
+                con5: "Uno de los dos pisos buscan gente joven, por lo que rechazan su petición sólo por su edad",
+                ima5: "cartoon-PCcrying.png",
+                
                 
                 /*** PASO #6: CONCLUSION ***/ 
                 
-                goal6: "Consiguie reservar para vacaciones pero no era lo que tenía en mente",
-                touch6: "Ordenador (reserva OK)",
-                feel6: "2",
-                con6: "Tendrá que buscar más información del lugar para ver que actividades ofrece y donde aparacar!",
-                ima6: "cartoon-PChard.png",
+                goal6: "Consigue reservar en el otro piso",
+                touch6: "ordenador",
+                feel6: "4",
+                con6: "Le molestó el trato recibido por las otras personas, pero está contento por haber encontrado piso",
+                ima6: "cartoon-phoning.png",
                 
                 
                 
